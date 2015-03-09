f0e7d71f9d8bb8bceee310939d5a3da54c2411f3
Andrew Shaffer <darawk@gmail.com>
2015-03-09 01:23:32 -0700

    immutable data

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 module.exports = function() {
-  this.get(function(value, type) {
-    return value === undefined
-      ? type.defaultValue
-      : value;
+  this.on('init', function(evt) {
+    var doc = evt.doc;
+    if(doc.value === undefined) {
+      doc.value = doc.model.defaultValue;
+    }
   });
-  
+
   this.default = function(value) {
     return this.use(function() {
       this.defaultValue = value;
